96900e1650bbd28b02579c113c46761230867126
Marina Samusenka <Marina.Samusenka@itechart-group.com>
2022-10-17 16:05:31 +0200

    change selector

--- cypress/e2e/google.cy.js
+++ cypress/e2e/google.cy.js
@@ -1,14 +1,14 @@
 describe('Test', () => {
   it('itechartcheck', () => {
     cy.visit('https://www.google.com/')
-    cy.get('#L2AGLb').click()
+    //cy.get('#L2AGLb').click()
     cy.get('[class="gLFyf gsfi"]').click().type('itechart{enter}')
 
-    cy.get('.v7W49e').children().as('firstpage')
-    cy.get('[aria-label="Page 2"]').click()
-    cy.get('.v7W49e').children().as('secondpage')
-
+    cy.get('#rso').children().as('firstpage')
     cy.get('@firstpage').each((item)=> { cy.wrap(item).contains('itechart', { matchCase: false })  })
+
+    cy.get('[aria-label="Page 2"]').click()
+    cy.get('#rso').children().as('secondpage')
     cy.get('@secondpage').each((item)=> { cy.wrap(item).contains('itechart', { matchCase: false }) })
     //expect(item).to.contain('itechart', { matchCase: false }) //не работает
   })
